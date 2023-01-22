88651070f1c9faed15fde8cc6c3493cd59b94506
Caua Pisoni <caua12.pisoni@gmail.com>
2023-01-21 22:23:44 -0300

    Routing update

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('angular');
 });
+Route::get('/app', function () {
+    return view('angular');
+});
+Route::get('/app/{angular}', function () {
+    return view('angular');
+})->where('angular','.*');
 
-Auth::routes();
+// Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
